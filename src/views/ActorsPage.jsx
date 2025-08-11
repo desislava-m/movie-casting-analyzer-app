eecfb4b31f27af4db46e08433d2a6f48a565b8dd
Dessie <desislava.m.alexandrova@gmail.com>
2025-08-11 21:05:07 +0300

    Styling ActorsPage

--- src/views/ActorsPage.jsx
+++ src/views/ActorsPage.jsx
@@ -8,18 +8,25 @@ export default function ActorsPage() {
     const { actors } = useContext(DataContext);
 
     return (
-        <>
-        <Navbar />
-        <h1>All actors:</h1>
-        <ul>
-            {actors.map((actor) => {
-                return (
-                    <li key={actor.id}>
-                        <Link to={`/actor/${actor.id}`}>{actor.fullname}</Link>
-                    </li>
-                )
-            })}
-        </ul>
-        </>
+        <div className="actor-list-container">
+            
+            <Navbar />
+            <div className="image-list-container">
+                <div className="list-container">
+                    <h1>All actors:</h1>
+                    <ul className="list">
+                        {actors.map((actor) => {
+                            return (
+                                <li key={actor.id}>
+                                    <Link to={`/actor/${actor.id}`}>{actor.fullname}</Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <div className="image-container">
+                </div>   
+            </div>   
+        </div>
     )
 }
